df9caa1c9ccdeee7ec89bc9f701ca10f709cb3d9
5Akan <akanimohsobot@gmail.com>
2025-03-11 17:07:45 +0100

    Put in view more and less toggle and put in dates in the array whilst starting newsletter component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,14 +3,30 @@ import { Link } from "react-router-dom";
 function Home(props) {
 
 const articles = props.articles;
+const [articleCount, setArtilcleCount] = useState(4);//sets articleCount to 4
 
-const articleList = articles.map(article=>
+
+function handleToggleArticleLength() {
+    //if number of articles is 4 then when button is pressed 
+    //the function makes the whole article length(articles.length)
+    // to be displayed 
+    //when button is pressed and the full articles are being displayed 
+    // then it reduces it back to 4 articles
+    setArtilcleCount(articleCount === 4 ? articles.length : 4 )
+}
+
+
+const articleList = articles.slice(0 , articleCount).map(article=>
+    //slice makes number of articles that have been mapped between
+    // 0 to 4
     <li key = {article.id}>
         <h2>
-        <Link to={`/article/${article.id}`}>
-        {article.title}
+        <Link to={`/article/${article.id}`}> 
+        {/* '/article/1' if it is the first id*/}
+                {article.title}
         </Link>       
         </h2>
+        <p>{article.date}</p>
     </li>
     
 )
@@ -26,8 +42,11 @@ const articleList = articles.map(article=>
             <ul>
                 {articleList}
             </ul>
-            
-            <span>Date</span>
+           <p onClick={handleToggleArticleLength}>
+               {/* when clicked it fires the function that toggles between 
+               full articles and 4 articles displayed */}
+               {articleCount === 4? "View More Articles" : "View Less Articles"}
+           </p>
         </div>
        )
            

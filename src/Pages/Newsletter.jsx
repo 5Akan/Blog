482213bedc2e7b75eaebe6e4eb128f2f46b1479b
5Akan <akanimohsobot@gmail.com>
2025-03-12 23:50:14 +0100

    Just adding a repo

--- src/Pages/Newsletter.jsx
+++ src/Pages/Newsletter.jsx
@@ -3,17 +3,37 @@ import React, {useState} from "react";
 function Newsletter() {
 
     const [email, setEmail] = useState("");
+    const [error, setError] = useState("");
 
-    function displayEmail(e) {
-        setEmail(e.target.value)
-    }
+   function emailErrorCheck(email) {
+       const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+       return  regex.test(email);
+   }
+
+ 
+
+   function handleSubmitEmail(e) {
+    const newEmail = e.target.value;
+    setEmail(newEmail)
+       
+       if (emailErrorCheck(email)) {
+          setError("Correct email")
+       }else if(newEmail === ""){
+          setError("");
+       }else{
+        setError("Invalid Email");
+       }
+   }
 
     return(
         <>
         <h1>The newsletter </h1>
-        <input type="text" value={email} onChange = {displayEmail}/>
+        <form >
+        <input type="text" value={email} onChange = {handleSubmitEmail}/>
         <button >Submit</button>
-        <p>{email}</p>
+        {error && <p style={{color:error.includes("Invalid")?"red" : "green"}}>{error}</p>}
+        </form>
+       
         </>)
          
     
